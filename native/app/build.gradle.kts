b896da0eecbe32a11f927e2bd51be41378a2d60b
unknokable0 <socype@gmail.com>
2026-09-20 01:44:42 +0200

    Bump SOHR to Beta 3.0

--- native/app/build.gradle.kts
+++ native/app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.unknokable.videosohranenki"
         minSdk = 26
         targetSdk = 35
-        versionCode = 210
-        versionName = "2.1-beta"
+        versionCode = 300
+        versionName = "3.0-beta"
 
         buildConfigField("int", "TELEGRAM_API_ID", telegramApiId)
         buildConfigField("String", "TELEGRAM_API_HASH", "\"$telegramApiHash\"")
