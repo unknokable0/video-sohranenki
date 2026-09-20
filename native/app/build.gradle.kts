b5ca9b98ce7ce06e15e05c13a6532189abbac5f3
unknokable0 <socype@gmail.com>
2026-09-20 04:06:33 +0200

    Bump SOHR to 3.0.7 interaction polish

--- native/app/build.gradle.kts
+++ native/app/build.gradle.kts
@@ -1,4 +1,4 @@
-// Build marker: SOHR 3.0.6 visual polish
+// Build marker: SOHR 3.0.7 interaction polish
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -15,8 +15,8 @@ android {
         applicationId = "com.unknokable.videosohranenki"
         minSdk = 26
         targetSdk = 35
-        versionCode = 306
-        versionName = "3.0.6-beta"
+        versionCode = 307
+        versionName = "3.0.7-beta"
 
         buildConfigField("int", "TELEGRAM_API_ID", telegramApiId)
         buildConfigField("String", "TELEGRAM_API_HASH", "\"$telegramApiHash\"")
